c5d31d9fe184fa0a98a1f44e550f48da6f368182
faresabumariam <faresabumariam02@gmail.com>
2022-03-21 21:24:54 +0100

    Commiting rn

--- MineSweeperProject/src/App.java
+++ MineSweeperProject/src/App.java
@@ -6,15 +6,11 @@ import view.MinesweeperView;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
         MinesweeperView view = new MinesweeperView();
-        //Uncomment the lines below once your game model code is ready; don't forget to import your game model 
         PlayableMinesweeper model = new Minesweeper();
-        
         view.setGameModel(model);
         model.setGameStateNotifier(view);
 
-
-        
-        //model.startNewGame(Difficulty.EASY);
     }
 }
--- MineSweeperProject/src/model/Minesweeper.java
+++ MineSweeperProject/src/model/Minesweeper.java
@@ -3,16 +3,14 @@ package model;
 import model.AbstractMineSweeper;
 import model.AbstractTile;
 import model.Difficulty;
-import java.util.Random;
 
 import java.util.Random;
-
 
 public class Minesweeper extends AbstractMineSweeper {
 
     private static int row, col, explosionCount, flagCount;
     private static AbstractTile[][] grid;
-    private boolean firstTileRule=true;
+    private boolean firstTileRule;
     private Random rd;
 
     @Override
@@ -48,38 +46,17 @@ public class Minesweeper extends AbstractMineSweeper {
         this.flagCount = 0;
         grid = new AbstractTile[row][col];
         this.explosionCount = explosionCount;
-
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                grid[i][j] = generateEmptyTile();
-            }
-        }
-        Random rand = new Random();
-
-        while (explosionCount > 0) {
-            int upperBoundY = row;
-            int upperBoundX = col;
-            int intRandomY = rand.nextInt(upperBoundY);
-            int intRandomX = rand.nextInt(upperBoundX);
-
-            if (!grid[intRandomY][intRandomX].isExplosive()) {
-                grid[intRandomY][intRandomX] = generateExplosiveTile();
-                explosionCount--;
-            }
-        }
-
+        firstTileRule = true;
         this.viewNotifier.notifyNewGame(row, col);
     }
 
 
     @Override
     public void toggleFlag(int x, int y) {
-        if (grid[x][y].isFlagged()) {
-            grid[x][y].unflag();
-        }
-        else {
-            grid[x][y].flag();
+        if (grid[y][x].isFlagged()) {
+            unflag(x, y);
+        } else {
+            flag(x, y);
         }
 
     }
@@ -106,120 +83,85 @@ public class Minesweeper extends AbstractMineSweeper {
 
     @Override
     public void open(int x, int y) {
-        if(x< grid[0].length && y< grid.length && x>=0 && y>=0)
-        {
+        if (x < grid[0].length && y < grid.length && x >= 0 && y >= 0) {
 
-            grid[y][x].isOpened();
+            if (firstTileRule) {
+                deactivateFirstTileRule(); // not first time anymore after this
+                grid[y][x] = generateEmptyTile(); // first tile is empty
+                getTile(x, y).open();
 
-            boolean check = false;
-            if (firstTileRule)
-            {
-                firstTileRule = false;
-                grid[y][x].open();
 
-                if (grid[y][x].isExplosive()) {
-                    grid[y][x] = generateEmptyTile();
-                    rd = new Random();
-                    while (!check) {
-                        int randomIndexRows = rd.nextInt(grid.length);
-                        int randomIndexColumns = rd.nextInt(grid[0].length);
-                        if (randomIndexRows != y && randomIndexColumns != x && !grid[randomIndexRows][randomIndexColumns].isExplosive()) {
-                            check = true;
-                            grid[randomIndexRows][randomIndexColumns] = new ExplosiveTile();
-                            grid[randomIndexRows][randomIndexColumns]=generateExplosiveTile();
+                // everything else empty for now
+                for (int i = 0; i < row; i++) {
+                    for (int j = 0; j < col; j++) {
+                        if (!(j == y && i == x)) {
+                            grid[j][i] = generateEmptyTile();
                         }
-
                     }
                 }
 
-            }
-            else
-            {
-                grid[y][x].open();
-            }
+                Random rand = new Random();
+                // setting random bombs
+                while (explosionCount > 0) {
+                    int upperBoundY = row-1;
+                    int upperBoundX = col-1;
+                    int intRandomY = rand.nextInt(upperBoundY);
+                    int intRandomX = rand.nextInt(upperBoundX);
 
-
-            int counter=0;
-            for (int i = x-1; i <= x+1; i++)
-            {
-                for (int j = y-1; j <= y+1; j++)
-                {
-                    if(i>=0 && i< grid.length && j>=0 && j<grid[0].length)
-                    {
-                        if( grid[i][j].isExplosive())
-                        {
-                            counter +=1;
-                        }
-                        else{}
-
+                    if (!(upperBoundX == x && upperBoundY == y)) {
+                        grid[intRandomY][intRandomX] = generateExplosiveTile();
+                        System.out.println(intRandomX + " " + intRandomY + " ");
+                        explosionCount--;
                     }
-                    else{}
                 }
-            }
-            viewNotifier.notifyOpened(x,y,counter);
 
-            if(grid[x][y].isExplosive())
-            {
-                viewNotifier.notifyExploded(x,y);
+
+            } else {
+
+                if (!grid[y][x].isFlagged() && !grid[y][x].isOpened()) {
+                    grid[y][x].open();
+                }
+
+            }
+
+            if (getTile(x, y).isExplosive()) {
+                viewNotifier.notifyExploded(x, y);
                 viewNotifier.notifyGameLost();
+                return;
             }
+
+            else {
+                //get number of nearby bombs
+                int counter = 0;
+                for (int i = x - 1; i <= x + 1; i++) {
+                    for (int j = y - 1; j <= y + 1; j++) {
+                        if (i >= 0 && i < grid[0].length && j >= 0 && j < grid.length) {
+                            if (getTile(i, j).isExplosive()) {
+                                counter += 1;
+                            }
+                        }
+                    }
+                }
+
+                // open nearby empty tiles
+                if (counter == 0) {
+                    for (int i = x - 1; i <= x + 1; i++) {
+                        for (int j = y - 1; j <= y + 1; j++) {
+                            if (i >= 0 && i < grid[0].length && j >= 0 && j < grid.length) {
+                                if (!getTile(i, j).isOpened()) {
+                                    open(i, j);
+                                }
+                            }
+                        }
+                    }
+
+                }
+                viewNotifier.notifyOpened(x, y, counter);
+            }
+
 
         }
-
-        else{}
     }
-
-//        boolean firstOne = true;
-//
-//        if(firstTileRule)
-//        {
-//            if(firstOne)
-//            {
-//                if(grid[y][x].isExplosive() && x >= 0 && y >= 0 && x < grid.length && y < grid[0].length)
-//                {
-//                    grid[y][x]=generateEmptyTile();
-//                    grid[y][x].open();
-//                    this.viewNotifier.notifyOpened(x, y, getNearbyExplosives(x,y));
-//
-//                }
-//                firstOne=false;
-//            }
-//
-//            else
-//            {
-//                if (x >= 0 && y >= 0 && x < grid.length && y < grid[0].length) {
-//                    if (grid[y][x].isExplosive() && !grid[y][x].isFlagged()) {
-//
-//                        this.viewNotifier.notifyExploded(x, y);
-//
-//                    }
-//                    if (!grid[y][x].isExplosive() && !grid[y][x].isFlagged()) {
-//                        grid[y][x].open();
-//                        this.viewNotifier.notifyOpened(x, y, getNearbyExplosives(x,y));
-//                    }
-//                }
-//
-//            }
-//        }
-//
-//        else
-//        {
-//            if (x >= 0 && y >= 0 && x < grid.length && y < grid[0].length) {
-//                if (grid[y][x].isExplosive() && !grid[y][x].isFlagged()) {
-//                    grid[y][x].open();
-//                    this.viewNotifier.notifyExploded(x, y);
-//
-//                }
-//                if (!grid[y][x].isExplosive() && !grid[y][x].isFlagged()) {
-//                    grid[y][x].open();
-//                    this.viewNotifier.notifyOpened(x, y, getNearbyExplosives(x,y));
-//                }
-//            }
-//        }
-//
-//
-//    }
-
 
     @Override
     public void flag(int x, int y) {
@@ -242,7 +184,7 @@ public class Minesweeper extends AbstractMineSweeper {
 
     @Override
     public void deactivateFirstTileRule() {
-            firstTileRule=true;
+        firstTileRule = false;
     }
 
     @Override
@@ -257,30 +199,5 @@ public class Minesweeper extends AbstractMineSweeper {
         return newTile;
     }
 
-
-
-    public int getNearbyExplosives(int thisPosX, int thisPosY){
-
-        int countExp =0;
-        int MIN_X = 0, MIN_Y =0;
-        int MAX_X = col - 1, MAX_Y = row - 1;
-
-        int startPosX = (thisPosX - 1 < MIN_X) ? thisPosX : thisPosX-1;
-        int startPosY = (thisPosY - 1 < MIN_Y) ? thisPosY : thisPosY-1;
-        int endPosX =   (thisPosX + 1 > MAX_X) ? thisPosX : thisPosX+1;
-        int endPosY =   (thisPosY + 1 > MAX_Y) ? thisPosY : thisPosY+1;
-
-
-// See how many are alive
-        for (int rowNum=startPosY; rowNum<=endPosY; rowNum++) {
-            for (int colNum=startPosX; colNum<=endPosX; colNum++) {
-                if(getTile(endPosX,endPosY).isExplosive()){
-                    countExp++;
-                }
-            }
-        }
-
-        return countExp;
-    }
 
 }
